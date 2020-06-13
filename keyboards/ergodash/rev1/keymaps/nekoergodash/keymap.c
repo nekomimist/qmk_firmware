3c378aec3dc9654cf5e6ccb52e3f9742a16180a6
ISHIKURA Hiroyuki <nekomist@gmail.com>
2020-06-14 00:41:57 +0900

    Modify nekoergodash
    * Swap LBRC/RBRC and HOME/PGUP.

--- keyboards/ergodash/rev1/keymaps/nekoergodash/keymap.c
+++ keyboards/ergodash/rev1/keymaps/nekoergodash/keymap.c
@@ -24,9 +24,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    * |  `   |   1  |   2  |   3  |   4  |   5  | ESC  |                    |      |   6  |   7  |   8  |   9  |   0  |  =   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   Q  |   W  |   E  |   R  |   T  |   [  |                    |  ]   |   Y  |   U  |   I  |   O  |   P  | Bksp |
+   * | Tab  |   Q  |   W  |   E  |   R  |   T  | HOME |                    | PGUP |   Y  |   U  |   I  |   O  |   P  | Bksp |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Ctrl |   A  |   S  |   D  |   F  |   G  | HOME |                    | PGUP |   H  |   J  |   K  |   L  |   ;  |  '   |
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  |   [  |                    |  ]   |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | Bksp |                    | Enter|   N  |   M  |   ,  |   .  |   /  | Enter|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
@@ -35,19 +35,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_QWERTY] = LAYOUT( \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_ESC ,                        _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME,                        KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BSPC,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
-    ADJUST,  KC_ESC,  KC_LGUI, KC_LALT,          M_LOWER, KC_SPC , KC_SPC,        KC_SPC ,KC_SPC , M_RAISE,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_HOME,                        KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,                         KC_SPC , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
+    ADJUST,  KC_ESC,  KC_LGUI, KC_LALT,          M_LOWER, KC_BSPC, KC_SPC,        KC_SPC ,KC_ENT , M_RAISE,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
   ),
 
   /* Lower
    * ,----------------------------------------------------------------------------------------------------------------------.
    * |  ~   |   !  |   @  |   #  |   $  |   %  |      |                    | PSCR |   ^  |   &  |   *  |   (  |   )  |  Del |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * |  ~   |   !  |   @  |   #  |   $  |   %  |      |                    |      |   ^  |   &  |   *  |   (  |   )  |  Del  |
+   * |  ~   |   !  |   @  |   #  |   $  |   %  | END  |                    | PGDN |   ^  |   &  |   *  |   (  |   )  |  Del |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * |      |  F1  |  F2  |  F3  |  F4  |  F5  | END  |                    | PGDN |  F6  |   _  |   +  |   {  |   }  |  |   |
+   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |      |                    |      |  F6  |   _  |   +  |   {  |   }  |  |   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |                    |      |  F12 |      |   <  | Home | End  |      |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
@@ -56,8 +56,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_LOWER] = LAYOUT(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,                        KC_PSCR, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,                        _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_END ,                        KC_PGDN, KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_END,                         KC_PGDN, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                        _______, KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______,                        _______, KC_F12,  _______, _______, KC_HOME, KC_END,  _______, \
     KC_CAPS, _______, _______, _______,          _______, _______, _______,       _______,_______, _______,          KC_HOME, KC_PGDN, KC_PGUP, KC_END \
   ),
@@ -66,9 +66,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    * |  `   !   1  |   2  |   3  |   4  |   5  |      |                    | PSCR |   6  |   7  |   8  |   9  |   0  |  Del |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * |  `   !   1  |   2  |   3  |   4  |   5  |      |                    |      |   6  |   7  |   8  |   9  |   0  |  Del |
+   * |  `   !   1  |   2  |   3  |   4  |   5  | END  |                    | PGDN |   6  |   7  |   8  |   9  |   0  |  Del |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * |      |  F1  |  F2  |  F3  |  F4  |  F5  | END  |                    | PGDN |  F6  |   -  |   =  |   [  |   ]  |  \   |
+   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |      |                    |      |  F6  |   -  |   =  |   [  |   ]  |  \   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |                    |      |  F12 |      |      |PageDn|PageUp|      |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
@@ -77,8 +77,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_RAISE] = LAYOUT(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4 ,   KC_5,    _______,                        KC_PSCR, KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4 ,   KC_5,    _______,                        _______, KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
-    KC_TAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_END ,                        KC_PGDN, KC_F6,    KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4 ,   KC_5,    KC_END ,                        KC_PGDN, KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
+    KC_TAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                        _______, KC_F6,    KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
     KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______,                        _______, KC_F12,   _______, _______, KC_PGDN, KC_PGUP, _______, \
     KC_CAPS, _______, _______, _______,          _______, _______, _______,       _______,_______, _______,          KC_HOME, KC_PGDN, KC_PGUP, KC_END \
   ),
