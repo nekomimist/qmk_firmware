bb2745dceda5618a067d7465161a87cd444b768e
Hiroyuki Ishikura <nekomist@gmail.com>
2020-10-27 00:31:21 +0900

    Add nksn0evt

---
+++ keyboards/e3w2q/su120/rev1/keymaps/nksn0evt/readme.md
@@ -0,0 +1,1 @@
+# The keymap for nksn0evt (猫砂0技術検証試験機)
--- keyboards/e3w2q/su120/rev1/rules.mk
+++ keyboards/e3w2q/su120/rev1/rules.mk
@@ -62,7 +62,7 @@ BOOTLOADER = caterina
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -79,3 +79,5 @@ AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs (+400)
 SPLIT_KEYBOARD = yes
+
+DEFAULT_FOLDER = e3w2q/su120/rev1
