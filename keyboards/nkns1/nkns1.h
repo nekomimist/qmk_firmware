34dcf210832cc23539e553050b27f377d6f8e6c7
Hiroyuki Ishikura <nekomist@gmail.com>
2021-02-06 22:25:38 +0900

    Modify nkns1
    * Move existing nekonos1 files to nkns1/rev01.
    * Create nkns1/rev02.

--- keyboards/nkns1/nkns1.h
+++ keyboards/nkns1/nkns1.h
@@ -1,56 +1,10 @@
-/* Copyright 2019 e3w2q
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #pragma once
 
 #include "quantum.h"
 
-/* This a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
-
-#define LAYOUT( \
-    l00, l01, l02, l03, l04, l05, l06, l07, l08, l09, \
-    l10, l11, l12, l13, l14, l15, l16, l17, l18, l19, \
-    l20, l21, l22, l23, l24, l25, l26, l27, l28, l29, \
-    l30, l31, l32, l33, l34, l35, l36, l37, l38, l39, \
-    l40, l41, l42, l43, l44, l45, l46, l47, l48, l49, \
-    l50, l51, l52, l53, l54, l55, l56, l57, l58, l59, \
-    r00, r01, r02, r03, r04, r05, r06, r07, r08, r09, \
-    r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, \
-    r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, \
-    r30, r31, r32, r33, r34, r35, r36, r37, r38, r39, \
-    r40, r41, r42, r43, r44, r45, r46, r47, r48, r49, \
-    r50, r51, r52, r53, r54, r55, r56, r57, r58, r59 \
-  ) \
-  { \
-    { l00, l01, l02, l03, l04, l05, l06, l07, l08, l09 }, \
-    { l10, l11, l12, l13, l14, l15, l16, l17, l18, l19 }, \
-    { l20, l21, l22, l23, l24, l25, l26, l27, l28, l29 }, \
-    { l30, l31, l32, l33, l34, l35, l36, l37, l38, l39 }, \
-    { l40, l41, l42, l43, l44, l45, l46, l47, l48, l49 }, \
-    { l50, l51, l52, l53, l54, l55, l56, l57, l58, l59 }, \
-    { r07, r06, r05, r04, r03, r02, r01, r00, r08, r09 }, \
-    { r17, r16, r15, r14, r13, r12, r11, r10, r18, r19 }, \
-    { r27, r26, r25, r24, r23, r22, r21, r20, r28, r29 }, \
-    { r37, r36, r35, r34, r33, r32, r31, r30, r38, r39 }, \
-    { r47, r46, r45, r44, r43, r42, r41, r40, r48, r49 }, \
-    { r57, r56, r55, r54, r53, r52, r51, r50, r58, r59 } \
-  }
+#ifdef KEYBOARD_nkns1_rev01
+#include "rev01.h"
+#endif
+#ifdef KEYBOARD_nkns1_rev02
+#include "rev02.h"
+#endif
--- keyboards/nkns1/rev02/keymaps/default/config.h
+++ keyboards/nkns1/rev02/keymaps/default/config.h
@@ -1,5 +1,8 @@
 /*
-Copyright 2021 nekomimist.org
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,4 +20,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
+// place overrides here
+#define IGNORE_MOD_TAP_INTERRUPT // トライ中
+#define PERMISSIVE_HOLD          // 同時押し時に早めにHOLD動作をさせるならON
+#define TAPPING_FORCE_HOLD       // SFT_T()を使うなら無効のほうがよさげ
+#define RETRO_TAPPING            // TAPPING_TERMが短いなら有効のほうがよさげ
+#undef  TAPPING_TERM
+#define TAPPING_TERM 100		 // RETRO_TAPPINGが有効なら短かくてもいい
